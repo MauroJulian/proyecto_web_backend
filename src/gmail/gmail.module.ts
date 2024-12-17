5b54133b1882e6ceec22454872b377e3cd18c42e
MauroJulian <139520502+MauroJulian@users.noreply.github.com>
2024-12-17 16:12:29 -0500

    deploy

---
+++ src/gmail/gmail.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { GmailService } from './gmail.service';
+import { GmailController } from './gmail.controller';
+
+@Module({
+  controllers: [GmailController],
+  providers: [GmailService],
+})
+
+export class GmailModule {}
